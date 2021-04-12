c8c09fa6c0f391678049004f882d8745adae77cf
GamerCoder215 <gamercoder215@gmail.com>
2021-04-11 19:57:08 -0500

    1.2 Updates

--- src/NodebookError.js
+++ src/NodebookError.js
@@ -2,9 +2,8 @@ class NodebookError extends Error {
 	constructor(message) {
 		super(message);
 		this.name = 'NodebookError';
-		this.stack = <call stack>
 	}
 }
 module.exports = {
 	NodebookError
-}
+};
